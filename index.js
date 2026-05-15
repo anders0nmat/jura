1c7cc322c5830b9b9e1d965a37e35b6cd0a5899f
Arian Schnappauf <arian@schnappi-witzschdorf.de>
2026-05-15 22:55:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const API_BASE = "https://de.openlegaldata.io/";
+function enableCors(url) {
+    return `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`;
+}
+async function fetchLaws() {
+    const url = enableCors("https://de.openlegaldata.io/api/laws/?book__latest=true&book__slug=gg&limit=5");
+    return fetch(url)
+        .then(e => {
+        return e.text();
+    })
+        .then(e => {
+        document.getElementById("logs").textContent += "API Result is " + e;
+        return JSON.parse(e);
+    })
+        .catch(e => document.getElementById("logs").textContent += e.toString());
+}
+async function fetchApi(endpoint, args = {}) {
+    const components = new URLSearchParams(args);
+    const url = API_BASE + 'api/' + endpoint + components.toString();
+    const corsUrl = enableCors(url);
+    return fetch(corsUrl).then(e => e.json());
+}
+const lawCard = document.getElementById("law");
+function loadLaw(id) {
+    fetchApi(`laws/${id}`)
+        .then(e => {
+        lawCard.dataset.id = e.id;
+        lawCard.querySelector("h4").textContent = e.title;
+        lawCard.querySelector("#body").innerHTML = e.content;
+    });
+}
+loadLaw(154853);
+document.addEventListener("keydown", e => {
+    if (e.key == "ArrowLeft") {
+        const currentLaw = parseInt(lawCard.dataset.id);
+        loadLaw(currentLaw - 1);
+    }
+    else if (e.key == "ArrowRight") {
+        const currentLaw = parseInt(lawCard.dataset.id);
+        loadLaw(currentLaw + 1);
+    }
+});
