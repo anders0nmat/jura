c8fd4f6372262040b4bb9b2ac4a669d7dec467a7
anders0nmat <fullgamefun@gmail.com>
2026-05-18 09:51:21 +0200

    change api, build search

--- index.js
+++ index.js
@@ -1,42 +1,213 @@
-const API_BASE = "https://de.openlegaldata.io/";
+function debounce(func, wait, immediate = false) {
+    var timeout;
+    return function (...args) {
+        var context = this;
+        var later = function () {
+            timeout = null;
+            if (!immediate)
+                func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout ?? undefined);
+        timeout = setTimeout(later, wait);
+        if (callNow)
+            func.apply(context, args);
+    };
+}
 function enableCors(url) {
-    return `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`;
+    return `https://api.codetabs.com/v1/proxy/?quest=${encodeURIComponent(url)}`;
 }
-async function fetchLaws() {
-    const url = enableCors("https://de.openlegaldata.io/api/laws/?book__latest=true&book__slug=gg&limit=5");
-    return fetch(url)
-        .then(e => {
-        return e.text();
-    })
-        .then(e => {
-        document.getElementById("logs").textContent += "API Result is " + e;
-        return JSON.parse(e);
-    })
-        .catch(e => document.getElementById("logs").textContent += e.toString());
-}
-async function fetchApi(endpoint, args = {}) {
-    const components = new URLSearchParams(args);
-    const url = API_BASE + 'api/' + endpoint + components.toString();
-    const corsUrl = enableCors(url);
-    return fetch(corsUrl).then(e => e.json());
-}
-const lawCard = document.getElementById("law");
-function loadLaw(id) {
-    fetchApi(`laws/${id}`)
-        .then(e => {
-        lawCard.dataset.id = e.id;
-        lawCard.querySelector("h4").textContent = e.title;
-        lawCard.querySelector("#body").innerHTML = e.content;
+const lawbooks = [];
+const law = document.getElementById('law');
+const lawbook = document.querySelector('h2');
+const lawbookLong = document.querySelector('h3');
+const search = document.querySelector('#book-search');
+const searchResults = document.querySelector('#search-results');
+let active_book;
+let active_law = 0;
+async function load_books() {
+    const response = await fetch(enableCors('https://gadi.netlify.app/'));
+    if (!response.ok) {
+        return;
+    }
+    const bodyHtml = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(bodyHtml, 'text/html');
+    doc.querySelectorAll('main ul:nth-of-type(2) li a').forEach(e => {
+        const href = e.href.match(/([^/]+)\.[^/\.]+$/)?.[1] ?? '';
+        const lawTitle = e.parentElement?.childNodes[3].textContent;
+        lawbooks.push([e.textContent, lawTitle ?? '', href]);
     });
 }
-loadLaw(154853);
-document.addEventListener("keydown", e => {
-    if (e.key == "ArrowLeft") {
-        const currentLaw = parseInt(lawCard.dataset.id);
-        loadLaw(currentLaw - 1);
+async function load_law(slug) {
+    const response = await fetch(enableCors('https://gadi.netlify.app/laws/' + slug + '.json'));
+    const json = await response.json();
+    active_book = json.data;
+    active_book.contents = active_book.contents.filter(e => e.type == "article");
+    active_law = 0;
+    display_law();
+}
+function display_law() {
+    const l = active_book.contents[active_law];
+    law.querySelector('h4').textContent = l.name + (l.title ? " " + l.title : "");
+    law.querySelector('#body').innerHTML = l.type == "article" ? l.body : '';
+    if (active_book.titleShort) {
+        lawbook.textContent = `${active_book.titleShort} (${active_book.abbreviation})`;
+        lawbookLong.textContent = active_book.titleLong;
+        lawbookLong.style.display = '';
     }
-    else if (e.key == "ArrowRight") {
-        const currentLaw = parseInt(lawCard.dataset.id);
-        loadLaw(currentLaw + 1);
+    else {
+        lawbook.textContent = `${active_book.titleLong} (${active_book.abbreviation})`;
+        lawbookLong.style.display = 'none';
+    }
+}
+load_books();
+await load_law('gg');
+document.addEventListener('keydown', e => {
+    if (e.defaultPrevented) {
+        return;
+    }
+    if (e.target instanceof HTMLInputElement) {
+        return;
+    }
+    if (e.key.length == 1) {
+        search.value = '';
+        search.focus();
+    }
+    if (e.key == "ArrowLeft") {
+        active_law -= 1;
+        if (active_law < 0) {
+            active_law = 0;
+        }
+        display_law();
+    }
+    if (e.key == "ArrowRight") {
+        const m = active_book.contents.length;
+        active_law += 1;
+        if (active_law >= m) {
+            active_law = m - 1;
+        }
+        display_law();
     }
 });
+search.addEventListener('input', debounce(_ => {
+    const value = search.value.toLowerCase();
+    searchResults.replaceChildren();
+    if (!value) {
+        return;
+    }
+    const priorities = new Map();
+    // Is there some matching lawitem in the current book?
+    active_book.contents.forEach((e, idx) => {
+        let score = 0;
+        if (e.name.toLowerCase().includes(value)) {
+            score += 1;
+        }
+        if (e.name.match(`\b${RegExp.escape(value)}\b}`)) {
+            score += 100;
+        }
+        if (score > 0) {
+            const li = document.createElement('span');
+            li.textContent = e.name + ' ' + (e.title ?? '');
+            li.dataset.index = idx.toString();
+            if (!priorities.has(-score)) {
+                priorities.set(-score, []);
+            }
+            priorities.get(-score)?.push(li);
+        }
+    });
+    // is there a matching law book?
+    lawbooks.forEach(e => {
+        let score = 0;
+        const content = e[0].toLowerCase() + ' ' + e[1].toLowerCase();
+        if (content.includes(value)) {
+            score += 1;
+        }
+        if (value && content.startsWith(value)) {
+            score += 10;
+        }
+        if (score > 0) {
+            const li = document.createElement('span');
+            li.textContent = e[0];
+            if (e[1]) {
+                const li2 = document.createElement('span');
+                li2.textContent = e[1];
+                li.append(li2);
+            }
+            li.dataset.slug = e[2];
+            if (!priorities.has(score)) {
+                priorities.set(score, []);
+            }
+            priorities.get(score)?.push(li);
+        }
+    });
+    const prioClass = Array.from(priorities.keys()).sort((a, b) => {
+        const aPos = a < 0 ? 0 : 1;
+        const bPos = b < 0 ? 0 : 1;
+        if (aPos !== bPos) {
+            return aPos - bPos;
+        }
+        return Math.abs(b) - Math.abs(a);
+    });
+    prioClass.forEach(prio => {
+        searchResults.append(...priorities.get(prio));
+    });
+    if (searchResults.firstElementChild) {
+        searchResults.firstElementChild.id = 'selected';
+    }
+}, 100));
+search.addEventListener('keydown', e => {
+    if (e.key == "ArrowDown") {
+        e.preventDefault();
+        const sel = searchResults.querySelector('#selected');
+        if (sel) {
+            sel.id = '';
+            if (sel.nextElementSibling) {
+                sel.nextElementSibling.id = 'selected';
+            }
+            else {
+                // end of list
+                searchResults.firstElementChild.id = 'selected';
+            }
+        }
+    }
+    if (e.key == "ArrowUp") {
+        e.preventDefault();
+        const sel = searchResults.querySelector('#selected');
+        if (sel) {
+            sel.id = '';
+            if (sel.previousElementSibling) {
+                sel.previousElementSibling.id = 'selected';
+            }
+            else {
+                // end of list
+                searchResults.lastElementChild.id = 'selected';
+            }
+        }
+    }
+    if (e.key == "Enter") {
+        const sel = searchResults.querySelector('#selected');
+        if (sel) {
+            if ('index' in sel.dataset) {
+                active_law = parseInt(sel.dataset.index);
+                display_law();
+                search.blur();
+            }
+            else if ('slug' in sel.dataset) {
+                load_law(sel.dataset.slug).then(_ => {
+                    active_law = 0;
+                    display_law();
+                    search.blur();
+                });
+            }
+        }
+    }
+    if (e.key == "Escape") {
+        search.blur();
+    }
+});
+search.addEventListener('blur', _ => {
+    search.value = '';
+    searchResults.replaceChildren();
+});
+export {};
--- index.ts
+++ index.ts
@@ -1,51 +1,295 @@
 
-const API_BASE = "https://de.openlegaldata.io/"
+export {}
 
-function enableCors(url) {
-    return `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`
+declare global {
+    interface RegExpConstructor {
+        escape(str: string): string;
+    }
 }
 
-async function fetchLaws() {
-    const url = enableCors("https://de.openlegaldata.io/api/laws/?book__latest=true&book__slug=gg&limit=5")
-    return fetch(url)
-        .then(e => {
-            return e.text()
-        })
-        .then(e => {
-            document.getElementById("logs").textContent += "API Result is " + e
-            return JSON.parse(e)
-        })
-        .catch(e => document.getElementById("logs").textContent += e.toString())
+interface PublicationInfo {}
+interface StatusInfo {}
+interface Notes {
+    body: string | null
+    footnotes: string | null
+    documentaryFootnotes: string | null
 }
 
-async function fetchApi(endpoint: string, args: {[key:string]: string} = {}): Promise<any> {
-    const components = new URLSearchParams(args)
-    const url = API_BASE + 'api/' + endpoint + components.toString()
-    const corsUrl = enableCors(url)
-    return fetch(corsUrl).then(e => e.json())
+interface LawItem {
+    type: string
+    id: string
+    name: string
+    title: string
+    parent: {
+        type: string
+        id: string
+    } | null
+    footnotes: string | null
+    documentaryFootnotes: string | null
 }
 
-const lawCard = document.getElementById("law")
-
-function loadLaw(id: number) {
-    fetchApi(`laws/${id}`)
-        .then(e => {
-            lawCard.dataset.id = e.id
-            lawCard.querySelector("h4").textContent = e.title
-            lawCard.querySelector("#body").innerHTML = e.content
-        })
+interface Heading extends LawItem {
+    type: "heading"
 }
 
-loadLaw(154853)
+interface Article extends LawItem {
+    type: "article"
+    body: string
+}
 
-document.addEventListener("keydown", e => {
+interface Law {
+    type: "law"
+    id: string
+    firstPublished: string
+    sourceTimestamp: string
+    titleShort: string | null
+    titleLong: string
+    abbreviation: string
+    slug: string
+    extraAbbreviation: string[]
+    publicationInfo: PublicationInfo[]
+    statusInfo: StatusInfo[]
+    notes: Notes
+    attachments: any
+    contents: (Heading | Article)[]
+}
+
+function debounce<Args extends any[], F extends (...args: Args) => any>(func: F, wait: number, immediate: boolean = false) {
+    var timeout: ReturnType<typeof setTimeout> | null
+    return function(this: ThisParameterType<F>, ...args: Parameters<F>) {
+		var context = this
+        var later = function() {
+            timeout = null
+            if (!immediate) func.apply(context, args)
+        }
+        var callNow = immediate && !timeout
+        clearTimeout(timeout ?? undefined)
+        timeout = setTimeout(later, wait)
+        if (callNow) func.apply(context, args)
+    }
+}
+
+function enableCors(url: string) {
+    return `https://api.codetabs.com/v1/proxy/?quest=${encodeURIComponent(url)}`
+}
+
+const lawbooks: [string, string, string][] = []
+const law = document.getElementById('law')!
+const lawbook = document.querySelector('h2')!
+const lawbookLong = document.querySelector('h3')!
+const search = document.querySelector<HTMLInputElement>('#book-search')!
+const searchResults = document.querySelector<HTMLElement>('#search-results')!
+let active_book: Law
+let active_law = 0
+
+
+async function load_books() {
+    const response = await fetch(enableCors('https://gadi.netlify.app/'))
+
+    if (!response.ok) {
+        return
+    }
+    const bodyHtml = await response.text()
+
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(bodyHtml, 'text/html')
+
+    doc.querySelectorAll<HTMLAnchorElement>('main ul:nth-of-type(2) li a').forEach(e => {
+        const href = e.href.match(/([^/]+)\.[^/\.]+$/)?.[1] ?? ''
+        const lawTitle = e.parentElement?.childNodes[3].textContent
+        lawbooks.push([e.textContent, lawTitle ?? '', href])
+    })
+}
+
+async function load_law(slug: string) {
+    const response = await fetch(enableCors('https://gadi.netlify.app/laws/' + slug + '.json'))
+    const json = await response.json()
+    active_book = json.data
+    active_book.contents = active_book.contents.filter(e => e.type == "article")
+    active_law = 0
+    display_law()
+}
+
+function display_law() {
+    const l = active_book.contents[active_law]
+    law.querySelector('h4')!.textContent = l.name + (l.title ? " " + l.title : "")
+    law.querySelector('#body')!.innerHTML = l.type == "article" ? l.body : ''
+
+    if (active_book.titleShort) {
+        lawbook.textContent = `${active_book.titleShort} (${active_book.abbreviation})`
+        lawbookLong.textContent = active_book.titleLong
+        lawbookLong.style.display = ''
+    }
+    else {
+        lawbook.textContent = `${active_book.titleLong} (${active_book.abbreviation})`
+        lawbookLong.style.display = 'none'
+    }
+}
+
+
+
+load_books()
+await load_law('gg')
+
+document.addEventListener('keydown', e => {
+    if (e.defaultPrevented) { return }
+    if (e.target instanceof HTMLInputElement) { return }
+
+    if (e.key.length == 1) {
+        search.value = ''
+        search.focus()
+    }
+
     if (e.key == "ArrowLeft") {
-        const currentLaw = parseInt(lawCard.dataset.id)
-        loadLaw(currentLaw - 1)
+        active_law -= 1
+        if (active_law < 0) {
+            active_law = 0
+        }
+        display_law()
     }
-    else if (e.key == "ArrowRight") {
-        const currentLaw = parseInt(lawCard.dataset.id)
-        loadLaw(currentLaw + 1)
+
+    if (e.key == "ArrowRight") {
+        const m = active_book.contents.length
+        active_law += 1
+        if (active_law >= m) {
+            active_law = m - 1
+        }
+        display_law()
     }
+})
+
+search.addEventListener('input', debounce(_ => {
+    const value = search.value.toLowerCase()
+
+    searchResults.replaceChildren()
+
+    if (!value) { return }
+
+    const priorities = new Map<number, HTMLElement[]>()
+
+    // Is there some matching lawitem in the current book?
+    active_book.contents.forEach((e, idx) => {
+        let score = 0
+        if (e.name.toLowerCase().includes(value)) {
+            score += 1
+        }
+
+        if (e.name.match(`\b${RegExp.escape(value)}\b}`)) {
+            score += 100
+        }
+
+        if (score > 0) {
+            const li = document.createElement('span')
+            li.textContent = e.name + ' ' + (e.title ?? '')
+            li.dataset.index = idx.toString()
+            if (!priorities.has(-score)) {
+                priorities.set(-score, [])
+            }
+            priorities.get(-score)?.push(li)
+        }
+    })
+
+    // is there a matching law book?
+    lawbooks.forEach(e => {
+        let score = 0
+        const content = e[0].toLowerCase() + ' ' + e[1].toLowerCase()
+        if (content.includes(value)) {
+            score += 1
+        }
+        if (value && content.startsWith(value)) {
+            score += 10
+        }
+
+        if (score > 0) {
+            const li = document.createElement('span')
+            li.textContent = e[0]
+            if (e[1]) {
+                const li2 = document.createElement('span')
+                li2.textContent = e[1]
+                li.append(li2)
+            }
+            li.dataset.slug = e[2]
+
+            if (!priorities.has(score)) {
+                priorities.set(score, [])
+            }
+            priorities.get(score)?.push(li)
+        }
+    })
+
+    const prioClass = Array.from(priorities.keys()).sort((a, b) => {
+        const aPos = a < 0 ? 0 : 1
+        const bPos = b < 0 ? 0 : 1
+        if (aPos !== bPos) { return aPos - bPos}
+
+        return Math.abs(b) - Math.abs(a)
+    })
+
+    prioClass.forEach(prio => {
+        searchResults.append(...priorities.get(prio)!)
+    })
+
+    if (searchResults.firstElementChild) {
+        searchResults.firstElementChild.id = 'selected'
+    }
+}, 100))
+
+search.addEventListener('keydown', e => {
+    if (e.key == "ArrowDown") {
+        e.preventDefault()
+        const sel = searchResults.querySelector<HTMLElement>('#selected')
+        if (sel) {
+            sel.id = ''
+            if (sel.nextElementSibling) {
+                sel.nextElementSibling.id = 'selected'
+            }
+            else {
+                // end of list
+                searchResults.firstElementChild!.id = 'selected'
+            }
+        }
+    }
+
+    if (e.key == "ArrowUp") {
+        e.preventDefault()
+        const sel = searchResults.querySelector<HTMLElement>('#selected')
+        if (sel) {
+            sel.id = ''
+            if (sel.previousElementSibling) {
+                sel.previousElementSibling.id = 'selected'
+            }
+            else {
+                // end of list
+                searchResults.lastElementChild!.id = 'selected'
+            }
+        }
+    }
+
+    if (e.key == "Enter") {
+        const sel = searchResults.querySelector<HTMLElement>('#selected')
+        if (sel) {
+            if ('index' in sel.dataset) {
+                active_law = parseInt(sel.dataset.index!)
+                display_law()
+                search.blur()
+            }
+            else if ('slug' in sel.dataset) {
+                load_law(sel.dataset.slug!).then(_ => {
+                    active_law = 0
+                    display_law()
+                    search.blur()
+                })
+            }
+        }
+    }
+
+    if (e.key == "Escape") {
+        search.blur()
+    }
+})
+
+search.addEventListener('blur', _ => {
+    search.value = ''
+    searchResults.replaceChildren()
 })
 
